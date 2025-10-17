7c4da5f7bd660428e5c7e42b56a7ee06e85976c1
Jozi Coder <jozicoder@gmail.com>
2025-10-17 12:02:15 +0200

    Code Notes - type coercion, increment/decrement, compound assignment, unary operator

--- notes/javascriptFundamentals/index.js
+++ notes/javascriptFundamentals/index.js
@@ -5,9 +5,31 @@ let message = "Hello Javascript";
 
 // Slice - negative number
 let sentence = "Learning Javascript is fun";
-console.log(sentence.slice(9, -4));
+//console.log(sentence.slice(9, -4));
 
 // Trim
 // both sides
 let msg = "    Hello   ";
-console.log(msg.trim());
+//console.log(msg.trim());
+
+// Type Coercion
+//console.log("10" + "number");
+//console.log("10" + 7);
+//console.log("10" - 7);
+//console.log("abc" - 7);
+//console.log("abc" * 7);
+
+// Increment and decrement
+let x = 2;
+//console.log(++x);
+let y = 3;
+//console.log(y++);
+
+// Compound Assignment
+let num = 7;
+//console.log((num += 2));
+
+// Unary Operator
+let str = "42";
+console.log(typeof +str);
+console.log(typeof -str);
