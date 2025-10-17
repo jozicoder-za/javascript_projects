925889d6c7fd959636fa335ac35acedebdc57a07
Jozi Coder <jozicoder@gmail.com>
2025-10-17 11:54:12 +0200

    Make code notes - Slice (both, start, end) and trim

---
+++ notes/javascriptFundamentals/index.js
@@ -0,0 +1,13 @@
+// Slice
+let message = "Hello Javascript";
+//console.log(message.slice(2));
+//console.log(message.slice(0, 5));
+
+// Slice - negative number
+let sentence = "Learning Javascript is fun";
+console.log(sentence.slice(9, -4));
+
+// Trim
+// both sides
+let msg = "    Hello   ";
+console.log(msg.trim());
