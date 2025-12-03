9df5b80cf013915498f8949aab429b001191b66a
Jozi Coder <jozicoder@gmail.com>
2025-12-03 11:29:33 +0200

    regular expression test method

--- notes/javascriptFundamentals/index.js
+++ notes/javascriptFundamentals/index.js
@@ -1517,4 +1517,14 @@ button {
 
 // Regular Expression
 const regex = /freeCodeCamp/;
-console.log(typeof regex);
+const test = regex.test("e");
+console.log(test);
+
+console.log(regex.test("freeCodeCamp"));
+console.log(regex.test("freeCodeCamp is great"));
+console.log(regex.test("I love freeCodeCamp"));
+console.log(regex.test("freecodecamp"));
+console.log(regex.test("FREECODECAMP"));
+console.log(regex.test("free"));
+console.log(regex.test("code"));
+console.log(regex.test("camp"));
