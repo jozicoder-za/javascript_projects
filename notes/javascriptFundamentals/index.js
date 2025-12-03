3354d2d5e190f2f2898cf4b5ba40d2f31c8e0b52
Jozi Coder <jozicoder@gmail.com>
2025-12-03 11:02:45 +0200

    try...catch...finally

--- notes/javascriptFundamentals/index.js
+++ notes/javascriptFundamentals/index.js
@@ -1475,3 +1475,19 @@ button {
 //}
 
 //console.log(4 / 0);
+
+// try...catch...finally
+function processInput(input) {
+  if (typeof input !== "string") {
+    throw new TypeError(`Input must be a string`);
+  }
+  return input.toUpperCase();
+}
+
+try {
+  console.log("Starting to process input..");
+  const result = processInput(9);
+  console.log("Processed result: ", result);
+} catch (error) {
+  console.log("Error occurred: ", error.message);
+}
