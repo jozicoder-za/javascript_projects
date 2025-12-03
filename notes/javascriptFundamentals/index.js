94028af5edbf2c11f63cfb3934afcdc90af9fe96
Jozi Coder <jozicoder@gmail.com>
2025-12-03 11:06:30 +0200

    debugger statement

--- notes/javascriptFundamentals/index.js
+++ notes/javascriptFundamentals/index.js
@@ -1477,17 +1477,25 @@ button {
 //console.log(4 / 0);
 
 // try...catch...finally
-function processInput(input) {
-  if (typeof input !== "string") {
-    throw new TypeError(`Input must be a string`);
-  }
-  return input.toUpperCase();
-}
+//function processInput(input) {
+//if (typeof input !== "string") {
+//throw new TypeError(`Input must be a string`);
+//}
+//return input.toUpperCase();
+//}
 
-try {
-  console.log("Starting to process input..");
-  const result = processInput(9);
-  console.log("Processed result: ", result);
-} catch (error) {
-  console.log("Error occurred: ", error.message);
-}
+//try {
+//console.log("Starting to process input..");
+//const result = processInput(9);
+//console.log("Processed result: ", result);
+//} catch (error) {
+//console.log("Error occurred: ", error.message);
+//}
+
+// Debugger Statement
+
+let firstNumber = 5;
+let secondNumber = 10;
+debugger;
+let sum = firstNumber + secondNumber;
+console.log(sum);
