f70801fdb04a3a7c5130e0b89e05e833af451f67
Jozi Coder <jozicoder@gmail.com>
2025-12-05 10:13:35 +0200

    global modifier

--- notes/javascriptFundamentals/index.js
+++ notes/javascriptFundamentals/index.js
@@ -1549,6 +1549,19 @@ button {
 //console.log(regex);
 
 // modifiers - i - ignore case sensitive
-const regex = /freeCodeCamp/i;
+//const regex = /freeCodeCamp/i;
+//console.log(regex.test("freecodecamp"));
+//console.log(regex.test("FREECODECAMP"));
+
+//console.log(regex.test("FrEeCoDeCamp"));
+
+// Global modifier - g flag - match a pattern more than once
+const regex = /freeCodeCamp/gi;
+
+console.log(regex.test("freeCodeCamp"));
+console.log(regex.test("freeCodeCamp is great"));
+console.log(regex.test("I love freeCodeCamp"));
 console.log(regex.test("freecodecamp"));
-console.log(regex.test("FREECODECAMP"));
+console.log(regex.test("free"));
+console.log(regex.test("code"));
+console.log(regex.test("camp"));
