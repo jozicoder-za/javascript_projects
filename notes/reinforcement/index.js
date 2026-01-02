96e32054bcf9ab8401288a6ec401e67892c91271
Jozi Coder <jozicoder@gmail.com>
2026-01-02 15:36:56 +0200

    Data type and type coercion (addition and minus)

--- notes/reinforcement/index.js
+++ notes/reinforcement/index.js
@@ -1,9 +1,17 @@
 // Level 1 - JavaScript Fundamentals
 // Q1 - What will this log
-console.log(typeof "Hello");
-console.log(typeof 42);
-console.log(typeof true);
+//console.log(typeof "Hello");
+//console.log(typeof 42);
+//console.log(typeof true);
 
 // Q2 - What is the output
 let x;
 console.log(x);
+
+// Q3 - Data Type and Type Coercion
+console.log(1 + "2" + "2");
+console.log(1 + +"2" + "2");
+console.log(1 + -"1" + "2");
+console.log(+"1" + "1" + "2");
+console.log("A" - "B" + "2");
+console.log("A" - "B" + 2);
