075bb94aeee5d84ff178ba3af2a9b5e4c17c6cb1
Jozi Coder <jozicoder@gmail.com>
2026-01-07 13:08:19 +0200

    shortcircuiting

--- notes/reinforcement/index.js
+++ notes/reinforcement/index.js
@@ -90,16 +90,20 @@
 // While loop amplified
 
 // Switch statement with a twist
-const x = 2;
+//const x = 2;
 
-switch (x) {
-  case 1:
-    console.log("one");
-  case 2:
-    console.log("two");
-  case 3:
-    console.log("three");
-    break;
-  default:
-    console.log("default");
-}
+//switch (x) {
+//case 1:
+//console.log("one");
+//case 2:
+//console.log("two");
+//case 3:
+//console.log("three");
+//break;
+//default:
+//console.log("default");
+//}
+
+// Short Circuiting
+const result = false && console.log("Hello");
+console.log(result);
