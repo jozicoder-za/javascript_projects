838904e0f126c5aacf939a54c4966d576b7a6473
Jozi Coder <jozicoder@gmail.com>
2026-01-08 15:16:38 +0200

    setting function default values

--- notes/reinforcement/index.js
+++ notes/reinforcement/index.js
@@ -123,6 +123,13 @@
 //greet(null);
 
 // spread functionality withing a function
-const arr1 = [1, 2];
-const arr2 = [...arr1, 3, 4];
-console.log(arr2);
+//const arr1 = [1, 2];
+//const arr2 = [...arr1, 3, 4];
+//console.log(arr2);
+
+// setting function default values
+function foo(a, b = 2) {
+  console.log(a + b);
+}
+
+foo(3);
