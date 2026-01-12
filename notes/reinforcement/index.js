716dff579cc13886303af75970f7c35d9872037a
Jozi Coder <jozicoder@gmail.com>
2026-01-12 13:51:30 +0200

    for of values extaction

--- notes/reinforcement/index.js
+++ notes/reinforcement/index.js
@@ -221,7 +221,12 @@
 //console.log(item);
 //}
 
-for (let i = 2; i <= 13; i++) {
-  if (i % 2 === 0) continue;
-  console.log(i);
+//for (let i = 2; i <= 13; i++) {
+//if (i % 2 === 0) continue;
+//console.log(i);
+//}
+
+const fruits = ["Mango", "Pineapple", "Oranges"];
+for (const fruit of fruits) {
+  console.log(fruit);
 }
