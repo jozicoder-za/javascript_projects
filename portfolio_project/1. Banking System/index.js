f859db3c0af566b816c256767213613ac77aafd7
Jozi Coder <jozicoder@gmail.com>
2026-03-01 13:32:10 +0200

    Update the User data

--- portfolio_project/1. Banking System/index.js
+++ portfolio_project/1. Banking System/index.js
@@ -1,4 +1,10 @@
 const account = {
-  balance: 2500,
+  balance: 2600,
   transactions: [],
 };
+
+function updateUI() {
+  document.getElementById("balance").textContent = account.balance;
+  const transactionList = document.getElementById("transactionList");
+  transactionList.innerHTML = "";
+}
