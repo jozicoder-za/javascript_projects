a3b1e4cdef4b04455a88169db22e9606587118fc
Jozi Coder <jozicoder@gmail.com>
2026-03-01 13:34:51 +0200

    loop through the transactions

--- portfolio_project/1. Banking System/index.js
+++ portfolio_project/1. Banking System/index.js
@@ -7,4 +7,11 @@ function updateUI() {
   document.getElementById("balance").textContent = account.balance;
   const transactionList = document.getElementById("transactionList");
   transactionList.innerHTML = "";
+
+  account.transactions.forEach((transaction) => {
+    const div = document.createElement("div");
+    div.classList.add("transaction");
+    div.textContent = `${transaction.type.toUpperCase()} - R${transaction.amount}`;
+    transactionList.appendChild(div);
+  });
 }
