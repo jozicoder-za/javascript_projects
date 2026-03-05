d8915ac9bd4161ef36e2d7eeb2a339a5ff72fc33
Jozi Coder <jozicoder@gmail.com>
2026-03-05 10:44:29 +0200

    workout function

---
+++ portfolio_project/2. Fitness Tracker/index.js
@@ -0,0 +1,7 @@
+const workout = [];
+
+// Work out function
+function addWorkout() {
+  const type = document.getElementById("type").value;
+  const duration = Number(document.getElementById("duration").value);
+}
