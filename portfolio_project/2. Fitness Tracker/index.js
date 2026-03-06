2dd6f301a81a9d8e72b69b3622b48c980ce63c24
Jozi Coder <jozicoder@gmail.com>
2026-03-06 17:41:13 +0200

    calories variables, input varification

--- portfolio_project/2. Fitness Tracker/index.js
+++ portfolio_project/2. Fitness Tracker/index.js
@@ -4,4 +4,10 @@ const workout = [];
 function addWorkout() {
   const type = document.getElementById("type").value;
   const duration = Number(document.getElementById("duration").value);
+  const calories = Number(document.getElementById("calories").value);
+
+  if (duration <= 0 || calories <= 0) {
+    alert("Please enter valid numbers");
+    return;
+  }
 }
