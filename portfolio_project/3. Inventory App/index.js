8ad3e2c6e788f1f1d2749b9746c21b43e74f66d1
Jozi Coder <jozicoder@gmail.com>
2026-03-12 14:13:16 +0200

    function variable declaration

--- portfolio_project/3. Inventory App/index.js
+++ portfolio_project/3. Inventory App/index.js
@@ -2,4 +2,7 @@ const inventory = [];
 
 function addProduct() {
   const name = document.getElementById("name").value;
+  const price = document.getElementById("price").value;
+  const stock = document.getElementById("stock").value;
+  const category = document.getElementById("category").value;
 }
