1edae63e7ee91ba1adf8b22ef06132e915088f00
Jozi Coder <jozicoder@gmail.com>
2026-03-22 01:06:05 +0200

    declare variable for run function

---
+++ portfolio_project/4. Running Log Analyzer/index.js
@@ -0,0 +1,7 @@
+const runs = [];
+
+function addRun() {
+  const distance = Number(document.getElementById("distance").value);
+  const duration = Number(document.getElementById("duration").value);
+  const terrain = document.getElementById("terrain").value;
+}
