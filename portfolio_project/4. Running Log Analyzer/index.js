d65e327415a9fa54476182e1260eaf1e522bebc3
Jozi Coder <jozicoder@gmail.com>
2026-03-23 11:02:02 +0200

    add logic to insert positive numbers

--- portfolio_project/4. Running Log Analyzer/index.js
+++ portfolio_project/4. Running Log Analyzer/index.js
@@ -4,4 +4,8 @@ function addRun() {
   const distance = Number(document.getElementById("distance").value);
   const duration = Number(document.getElementById("duration").value);
   const terrain = document.getElementById("terrain").value;
+
+  if (distance <= 0 || duration <= 0) {
+    alert("Please enter  valid values");
+  }
 }
