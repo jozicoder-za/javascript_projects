a0a6a98f4c5b42b0a9aaaf7f13938d616d35d0cc
Jozi Coder <jozicoder@gmail.com>
2026-03-28 12:26:14 +0200

    create save data function

--- portfolio_project/5. Banking App - Local Storage/index.js
+++ portfolio_project/5. Banking App - Local Storage/index.js
@@ -2,3 +2,7 @@ let account = JSON.parse(localStorage.getItem("account")) || {
   balance: 2500,
   transactions: [],
 };
+
+function saveData() {
+  localStorage.setItem("account", JSON.stringify(account));
+}
