52d42b533795b831ef18298ae9104239a3bfdaca
Jozi Coder <jozicoder@gmail.com>
2026-03-29 12:41:58 +0200

    update function looping through the account

--- portfolio_project/5. Banking App - Local Storage/index.js
+++ portfolio_project/5. Banking App - Local Storage/index.js
@@ -11,4 +11,10 @@ function updateUI() {
   document.getElementById("balance").textContent = account.balance;
   const list = document.getElementById("list");
   list.innerHTML = "";
+
+  account.transactions.forEach((t) => {
+    const div = document.createElement("div");
+    div.textContent = `${t.type} -R${t.amount}`;
+    list.appendChild(div);
+  });
 }
