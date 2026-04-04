47caed2c8057d5e562fb73340f34276cea3f6422
Jozi Coder <jozicoder@gmail.com>
2026-04-04 14:38:36 +0200

    Add workout function and declare variables

--- portfolio_project/6. Fitness Tracker - Local Storage/index.js
+++ portfolio_project/6. Fitness Tracker - Local Storage/index.js
@@ -3,3 +3,9 @@ let workouts = JSON.parse(localStorage.getItem("workouts")) || [];
 function saveData() {
   localStorage.setItem("workouts", JSON.stringify(workouts));
 }
+
+function addWorkout() {
+  const type = document.getElementById("type").value;
+  const duration = Number(document.getElementById("duration").value);
+  const calories = Number(document.getElementById("calories").value);
+}
