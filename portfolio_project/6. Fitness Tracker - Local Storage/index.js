39d2e18bc1976ec9e4df7694f52a070fefa50700
Jozi Coder <jozicoder@gmail.com>
2026-04-05 15:07:38 +0200

    conditional statement to verify input data

--- portfolio_project/6. Fitness Tracker - Local Storage/index.js
+++ portfolio_project/6. Fitness Tracker - Local Storage/index.js
@@ -8,4 +8,9 @@ function addWorkout() {
   const type = document.getElementById("type").value;
   const duration = Number(document.getElementById("duration").value);
   const calories = Number(document.getElementById("calories").value);
+
+  if (duration <= 0 || calories <= 0) {
+    alert("Invalid input");
+    return;
+  }
 }
