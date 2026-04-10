0750297ae7846f72f8b1beecc9a627278396ee74
Jozi Coder <jozicoder@gmail.com>
2026-04-10 15:33:13 +0200

    adding product function variable declaration

--- portfolio_project/7. Inventory - Local Storage/index.js
+++ portfolio_project/7. Inventory - Local Storage/index.js
@@ -3,3 +3,10 @@ let inventory = JSON.parse(localStorage.getItem("inventory")) || [];
 function saveData() {
   localStorage.setItem("inventory", JSON.stringify(inventory));
 }
+
+function addProduct() {
+  const name = document.getElementById("name").value;
+  const price = Number(document.getElementById("price").value);
+  const stock = Number(document.getElementById("stock").value);
+  const category = document.getElementById("category").value;
+}
