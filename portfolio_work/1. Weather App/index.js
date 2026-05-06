753c78200648696c6f91862b43751f6290eea61b
Jozi Coder <jozicoder@gmail.com>
2026-05-06 18:52:47 +0200

    updating the DOM with real api data values

--- portfolio_work/1. Weather App/index.js
+++ portfolio_work/1. Weather App/index.js
@@ -6,6 +6,10 @@ async function checkWeather() {
   const response = await fetch(apiUrl + `&appid=${apiKey}`);
   let data = await response.json();
   console.log(data);
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = data.main.temp;
+  document.querySelector(".humidity").innerHTML = data.main.humidity;
+  document.querySelector(".wind").innerHTML = data.wind;
 }
 
 checkWeather();
