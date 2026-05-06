de6d1311701cfee3634070f4142d7e9fb1338eba
Jozi Coder <jozicoder@gmail.com>
2026-05-06 19:04:42 +0200

    add value measuring units and city search input

--- portfolio_work/1. Weather App/index.js
+++ portfolio_work/1. Weather App/index.js
@@ -1,15 +1,20 @@
 const apiKey = "5075c715d1cbf033e72f0647294dd972";
 const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=pretoria";
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather() {
-  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
   let data = await response.json();
   console.log(data);
   document.querySelector(".city").innerHTML = data.name;
-  document.querySelector(".temp").innerHTML = data.main.temp;
-  document.querySelector(".humidity").innerHTML = data.main.humidity;
-  document.querySelector(".wind").innerHTML = data.wind;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + " kn/hr";
 }
 
-checkWeather();
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
