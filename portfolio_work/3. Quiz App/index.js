dc6dde24e76b6a7bf4871685a6f1d990e90978f9
Jozi Coder <jozicoder@gmail.com>
2026-05-15 16:31:02 +0200

    create key applications variables

--- portfolio_work/3. Quiz App/index.js
+++ portfolio_work/3. Quiz App/index.js
@@ -36,3 +36,7 @@ const questions = [
     ],
   },
 ];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
