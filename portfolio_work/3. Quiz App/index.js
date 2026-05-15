83113afb523cf822f618fe3ba1f3a74b322dd8f7
Jozi Coder <jozicoder@gmail.com>
2026-05-15 16:26:56 +0200

    quiz questions and answers

--- portfolio_work/3. Quiz App/index.js
+++ portfolio_work/3. Quiz App/index.js
@@ -8,4 +8,31 @@ const questions = [
       { text: "Giraffe", correct: false },
     ],
   },
+  {
+    question: "Which is the smallest country in the world?",
+    answers: [
+      { text: "Vatican City", correct: true },
+      { text: "Bhutan", correct: false },
+      { text: "Nepal", correct: false },
+      { text: "Shri Lanka", correct: false },
+    ],
+  },
+  {
+    question: "Which is the largest dessert in the world?",
+    answers: [
+      { text: "Kalahari", correct: false },
+      { text: "Gobi", correct: false },
+      { text: "Sahara", correct: false },
+      { text: "Antarctica", correct: true },
+    ],
+  },
+  {
+    question: "Which is the smallest continent in the world?",
+    answers: [
+      { text: "Asia", correct: false },
+      { text: "Australia", correct: true },
+      { text: "Arctic", correct: false },
+      { text: "Africa", correct: true },
+    ],
+  },
 ];
