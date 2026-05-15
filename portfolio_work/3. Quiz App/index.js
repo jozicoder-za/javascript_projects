b222afe65caf8b2f12b144477e7d0ac5eafd05b4
Jozi Coder <jozicoder@gmail.com>
2026-05-15 17:26:33 +0200

    app tracking variables

--- portfolio_work/3. Quiz App/index.js
+++ portfolio_work/3. Quiz App/index.js
@@ -40,3 +40,13 @@ const questions = [
 const questionElement = document.getElementById("question");
 const answerButton = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestions();
+}
