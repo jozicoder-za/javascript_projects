d1ec3ab6b562d84cdab2c421ca76a0b0c39af86b
Jozi Coder <jozicoder@gmail.com>
2026-05-25 10:41:15 +0200

    create select answer function

--- portfolio_work/3. Quiz App/index.js
+++ portfolio_work/3. Quiz App/index.js
@@ -76,4 +76,15 @@ function resetState() {
   }
 }
 
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+}
 startQuiz();
+
+// continue 24:44
