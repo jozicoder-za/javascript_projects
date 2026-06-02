829c552bf00e34e3e3b038290caf8f53c75689dd
Jozi Coder <jozicoder@gmail.com>
2026-06-02 19:24:19 +0200

    password character number and symbol and function declaration

--- portfolio_work/4. Random Password Generator/index.js
+++ portfolio_work/4. Random Password Generator/index.js
@@ -3,3 +3,10 @@ const length = 12;
 
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+~|}{[]></-=";
+
+function createPassword() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+}
