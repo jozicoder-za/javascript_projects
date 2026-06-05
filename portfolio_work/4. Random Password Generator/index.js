4f8e9bae1cee12cada818039b0e064d97196d993
Jozi Coder <jozicoder@gmail.com>
2026-06-05 14:23:05 +0200

    add the event listener on the generage password button

--- portfolio_work/4. Random Password Generator/index.js
+++ portfolio_work/4. Random Password Generator/index.js
@@ -6,6 +6,7 @@ const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789";
 const symbol = "@#$%^&*()_+~|}{[]></-=";
 const allChars = upperCase + lowerCase + number + symbol;
+
 function createPassword() {
   let password = "";
   password += upperCase[Math.floor(Math.random() * upperCase.length)];
@@ -16,5 +17,17 @@ function createPassword() {
   while (length > password.length) {
     password += allChars[Math.floor(Math.random() * allChars.length)];
   }
-  password.value = password;
+  passwordBox.value = password;
 }
+
+const btnEl = document.getElementById("btn");
+btnEl.addEventListener("click", createPassword);
+
+/*
+  16:46
+
+  on the video they use the onclick but I will use 
+  the addEventlist on the btnEl
+  give the button an id
+
+*/
