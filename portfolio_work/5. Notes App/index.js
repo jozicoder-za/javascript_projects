3a5eb9a3800cdcdaa603316ef350b5a1595b95d8
Jozi Coder <jozicoder@gmail.com>
2026-06-15 19:43:30 +0200

    add create Notes button functionality

--- portfolio_work/5. Notes App/index.js
+++ portfolio_work/5. Notes App/index.js
@@ -1,4 +1,12 @@
 const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
-console.log(notes);
+
+createBtn.addEventListener("click", () => {
+  let inputBox = document.createElement("p");
+  let img = document.createElement("img");
+  inputBox.className = "input-box";
+  inputBox.setAttribute("contenteditable", "true");
+  img.src = "./media/delete.png";
+  notesContainer.appendChild(inputBox).appendChild(img);
+});
