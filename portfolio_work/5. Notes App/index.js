41f756d3212a761891c10e35ffbda400dde8688a
Jozi Coder <jozicoder@gmail.com>
2026-06-16 17:06:16 +0200

    add the local storage functionality to make notes persistent

--- portfolio_work/5. Notes App/index.js
+++ portfolio_work/5. Notes App/index.js
@@ -2,6 +2,10 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
+function updateStorage() {
+  localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
 createBtn.addEventListener("click", () => {
   let inputBox = document.createElement("p");
   let img = document.createElement("img");
@@ -14,5 +18,6 @@ createBtn.addEventListener("click", () => {
 notesContainer.addEventListener("click", (e) => {
   if (e.target.tagName === "IMG") {
     e.target.parentElement.remove();
+    updateStorage();
   }
 });
