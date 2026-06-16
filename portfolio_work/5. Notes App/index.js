c4e776936cd8940356e6f500a979e1f55299be19
Jozi Coder <jozicoder@gmail.com>
2026-06-16 17:03:18 +0200

    add the trash can functionality

--- portfolio_work/5. Notes App/index.js
+++ portfolio_work/5. Notes App/index.js
@@ -10,3 +10,9 @@ createBtn.addEventListener("click", () => {
   img.src = "./media/delete.png";
   notesContainer.appendChild(inputBox).appendChild(img);
 });
+
+notesContainer.addEventListener("click", (e) => {
+  if (e.target.tagName === "IMG") {
+    e.target.parentElement.remove();
+  }
+});
