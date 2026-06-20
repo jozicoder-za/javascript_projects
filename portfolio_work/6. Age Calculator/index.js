cc43a3048a64e2064091cb65062b0b5572eaa2c4
Jozi Coder <jozicoder@gmail.com>
2026-06-20 10:05:31 +0200

    Create Age Computation function

--- portfolio_work/6. Age Calculator/index.js
+++ portfolio_work/6. Age Calculator/index.js
@@ -1,1 +1,18 @@
-console.log("sifiso");
+let userInput = document.getElementById("date");
+userInput.max = new Date().toISOString().split("T")[0];
+const btnEl = document.querySelector(".btn");
+
+btnEl.addEventListener("click", () => {
+  let birthDate = new Date(userInput.value);
+  let d1 = birthDate.getDate();
+  let m1 = birthDate.getMonth() + 1;
+  let y1 = birthDate.getFullYear();
+
+  let today = new Date();
+
+  let d2 = today.getDate();
+  let m2 = today.getMonth() + 1;
+  let y2 = today.getFullYear();
+});
+
+/* continue 12:36 */
