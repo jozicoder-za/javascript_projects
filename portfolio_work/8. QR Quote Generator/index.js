ffe9f9978354bee703461c7894a165424dde8a66
Jozi Coder <jozicoder@gmail.com>
2026-07-06 12:27:02 +0200

    Create the button element and get the generic QR API url

--- portfolio_work/8. QR Quote Generator/index.js
+++ portfolio_work/8. QR Quote Generator/index.js
@@ -5,10 +5,16 @@
   URL =>  https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example   
   Change the code to Sifiso Dhlamini
    https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Sifiso_Dhlamini
-  */
+    Plain URL
+    https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=
+   */
 
 let imgBox = document.getElementById("imgBox");
 let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
-
-function generateQR() {}
+let btnEl = document.getElementById("btn");
+function generateQR() {
+  qrImage.src =
+    "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+    qrText.value;
+}
