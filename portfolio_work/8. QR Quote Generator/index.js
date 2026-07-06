1d95f91b48b8fd18eed73b58de8b02b4fc83858f
Jozi Coder <jozicoder@gmail.com>
2026-07-06 12:20:50 +0200

    QR code Api details

--- portfolio_work/8. QR Quote Generator/index.js
+++ portfolio_work/8. QR Quote Generator/index.js
@@ -1,1 +1,12 @@
+/*
+  www.google.com
+  qr code api
+  https://goqr.me/api/
+  URL =>  https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example   
+  Change the code to Sifiso Dhlamini
+   https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Sifiso_Dhlamini
+  */
 
+let imgBox = document.getElementById("imgBox");
+
+function generateQR() {}
