1117e2cbafaa20847d142e1b81f1a3a4af37d481
Jozi Coder <jozicoder@gmail.com>
2026-07-06 12:23:07 +0200

    QR Api variable declaration

--- portfolio_work/8. QR Quote Generator/index.js
+++ portfolio_work/8. QR Quote Generator/index.js
@@ -8,5 +8,7 @@
   */
 
 let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("qrText");
 
 function generateQR() {}
