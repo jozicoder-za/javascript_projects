425a271852b411995f7197d7e1614fdee6a18db6
Jozi Coder <jozicoder@gmail.com>
2026-09-17 13:20:22 +0200

    compile the stopwatch function

--- portfolio_august/1. Stopwatched/index.js
+++ portfolio_august/1. Stopwatched/index.js
@@ -2,3 +2,15 @@ let displayTime = document.getElementById("displayTime");
 
 let [seconds, minutes, hours] = [0, 0, 0];
 let timer = null;
+
+function stopwatch() {
+  seconds++;
+  if (seconds == 60) {
+    seconds = 0;
+    minutes++;
+    if (minutes == 60) {
+      minutes = 0;
+      hours++;
+    }
+  }
+}
